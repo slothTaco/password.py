2149e61f217ed225f753ac7ac2ef3786c015c4bf
slothTaco <pickeringwilliamscott@gmail.com>
2024-12-10 12:34:09 -0600

    Create password.py
    password for access

---
+++ password.py
@@ -0,0 +1,10 @@
+#Get user input
+print('Welcome')
+password = input('Enter your password:')
+#if wrong password
+#You are not allowed to access this program 
+if password == 'secret':
+    print('You can access this program')
+else:
+    print('You are not allowed to access this program')
+
